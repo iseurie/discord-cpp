82b834f124b13a409eb5b31635d6f90d50102f6b
iseurie@gmail.com <iseurie@gmail.com>
2017-03-05 14:20:28 -0500

    emojis non-fetchable: impl complete

--- src/emoji.hh
+++ src/emoji.hh
@@ -5,20 +5,34 @@
 
 namespace dsc {
 
-struct Emoji : Fetchable {
+struct Emoji : WAPIObject {
+    snowflake id;
     std::vector<snowflake> active_roles;
     bool require_colons, is_managed;
     std::string name;
-    
-    Emoji();
-    ~Emoji();
+
     rapidjson::Document serialize();
-    WAPIResult fetch(snowflake id);
-    WAPIError parse(const rapidjson::Document* v);
+    rapidjson::ParseResult parse(const rapidjson::Document* v);
 };
+
+rapidjson::ParseResult parse(const rapidjson::Document* v) {
+    id = d["id"].GetLong();
+    name = d["name"].GetString();
+    require_colons = d["require_colons"].GetBool();
+    managed = d["is_managed"].GetBool();
+    int arSize = d["active_roles"].Size();
+    active_roles.reserve(arSize);
+    for(int i = 0; i < arSize; i++) {
+        Role parsed;
+        rapidjson::ParseResult rpr = parsed.parse(d["active_roles"][i]);
+        if(rpr.IsError) return rpr;
+        active_roles.insert(i, parsed);
+    }
+}
 
 rapidjson::Document Emoji::serialize() {
     rapidjson::Document d;
+    d["id"] = id;
     d["name"] = name;
     d["require_colons"] = require_colons;
     d["is_managed"] = managed;
